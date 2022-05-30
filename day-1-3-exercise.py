013fe6342682a9e3715d47ab4154924a7a880e7b
Anil Magar <86339793+AnilRana675@users.noreply.github.com>
2022-05-30 06:35:38 +0545

    Add files via upload

---
+++ day-1-3-exercise.py
@@ -0,0 +1,8 @@
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
+
+#Notes
+#If input was "anil"
+#1st: print(len("anil"))
+#2nd: print(4)
